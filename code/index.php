bc56dcd7ab68463ac8b61fa6d4fa36a82c499bd2
sunnyfeijen <Sunny@MacBook-Pro-van-Sunny.local>
2015-05-31 19:16:44 +0200

    homepage slider / css / responsive / menu
    slider with hardcoded images

--- code/index.php
+++ code/index.php
@@ -12,40 +12,60 @@
 		<a href="#" class="control_next"></a>
 		<a href="#" class="control_prev"></a>
 -->
+<!--
 		<ul>
 
 			<?php
-				$con = mysqli_connect("localhost", "delta_website", "admin", "delta_website");
-
-				$query = "SELECT * FROM novo_project WHERE Featured = 1 LIMIT 0, 1";
-				if (!mysqli_query($con,$query))
-				{
-					die('Error: ' . mysqli_error($con));
-				}
-				else
-				{
-					$result = mysqli_query($con,$query);
-				}
-
-				while($row = mysqli_fetch_assoc($result))
-				{
-					$title = stripslashes($row['Name']);
-					$description = charLimit(stripslashes($row['Description']), 250);
-					echo '<li class="homeimage" style="background-image: url(images/project/' . $row['ProjectID'] . '.jpg);">
-								<div class="bannertitlebox"><div class="homedescription">
-									<a class="homelink" href="project#' . $row['ProjectID'] . '"><h1 class="bannertitle">' . $title . '</h1>
-									<p class="bannertext">
-										' . $description . '
-									</p></a>
-                                    </div>
-								</div>
-							</li>';
-				}
-
-				@mysqli_close($conn);
+//				$con = mysqli_connect("localhost", "delta_website", "admin", "delta_website");
+//
+//				$query = "SELECT * FROM novo_project WHERE Featured = 1 LIMIT 0, 1";
+//				if (!mysqli_query($con,$query))
+//				{
+//					die('Error: ' . mysqli_error($con));
+//				}
+//				else
+//				{
+//					$result = mysqli_query($con,$query);
+//				}
+//
+//				while($row = mysqli_fetch_assoc($result))
+//				{
+//					$title = stripslashes($row['Name']);
+//					$description = charLimit(stripslashes($row['Description']), 250);
+//					echo '<li class="homeimage" style="background-image: url(images/project/' . $row['ProjectID'] . '.jpg);">
+//								<div class="bannertitlebox"><div class="homedescription">
+//									<a class="homelink" href="project#' . $row['ProjectID'] . '"><h1 class="bannertitle">' . $title . '</h1>
+//									<p class="bannertext">
+//										' . $description . '
+//									</p></a>
+//                                    </div>
+//								</div>
+//							</li>';
+//				}
+//
+//				@mysqli_close($conn);
 			?>
 
 		</ul>
+-->
+<div class="wrapper">
+    <ul class="rslides" id="slider3">
+        <li><div style="background-image:url(images/1.jpg)" alt="" class="slide"></div></li>
+        <li><div style="background-image:url(images/2.jpg)" alt="" class="slide"></div></li>
+        <li><div style="background-image:url(images/3.jpg)" alt="" class="slide"></div></li>
+        <li><div style="background-image:url(images/4.jpg)" alt="" class="slide"></div></li>
+    </ul>
+
+    <!-- Slideshow 3 Pager -->
+    <ul id="slider3-pager">
+      <li><a href="#">.</a></li>
+      <li><a href="#">.</a></li>
+      <li><a href="#">.</a></li>
+      <li><a href="#">.</a></li>
+    </ul>
+</div>
+
+
 <!--	</section>-->
 <!--
 	<section class="block">
--- code/parts/header.php
+++ code/parts/header.php
@@ -8,10 +8,26 @@
 	<link rel="shortcut icon" type="image/x-icon" href="images/static/favicon.ico" />
 	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
 	<link rel="stylesheet" href="resources/style.css" type="text/css" />
+    <link rel="stylesheet" href="resources/slider.css" type="text/css" />
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 	<script type="text/javascript" src="http://code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
 	<script type="text/javascript" src="resources/animatescroll.js"></script>
+    <script type="text/javascript" src="resources/menuicon.js"></script>
+<!--    <script type="text/javascript" src="resources/responsiveslides.min.js"></script>-->
+    <script type="text/javascript" src="resources/responsiveslides.js"></script>
 	<script type="text/javascript" src="resources/source.js"></script>
+    
+    <script>
+    // You can also use "$(window).load(function() {"
+    $(function () {
+
+      // Slideshow 3
+      $("#slider3").responsiveSlides({
+        manualControls: '#slider3-pager'
+      });
+
+    });
+  </script>
 	<?php
 		if(isset($include))
 		{
@@ -30,6 +46,13 @@
             <li><a href="projects.php">Portfolio</a></li>
             <li><a href="contact.php">Contact</a></li>
         </ul>
+        
+        <div class="menuicon">
+          <button class="cmn-toggle-switch cmn-toggle-switch__htx">
+            <span>toggle menu</span>
+          </button>
+        </div>
+        
 	</header>
 
 	<?php
